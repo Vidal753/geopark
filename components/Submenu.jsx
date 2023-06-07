ca59352f64383173a89b2621102e07a5e5fa077e
Vidal Francisco  Baquedano <devbooster@Vidals-MacBook-Air.local>
2023-06-07 17:18:32 -0600

    submenu-created

---
+++ components/Submenu.jsx
@@ -0,0 +1,23 @@
+import '../styles/component/subMenu.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+
+const Submenu = ({ label, items }) => {
+  return (
+    <section className='submenu'>
+      <h3>{label}</h3>
+      <FontAwesomeIcon className='submenu__icon' icon={faChevronDown} />
+      <article className='submenu__container'>
+        {
+          items.map(item => (
+            <div key={item.value} className='submenu__item'>
+              <p className='submenu__label'>{item.label}</p>
+            </div>
+          ))
+        }
+      </article>
+    </section>
+  )
+}
+
+export default Submenu
