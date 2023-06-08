c933206a7059440b3e3f6f767404aac380bef1fd
Vidal Francisco  Baquedano <devbooster@Vidals-MacBook-Air.local>
2023-06-07 23:04:23 -0600

    InfoPage-responsive-implementation

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,7 @@ export default function RootLayout ({ children }) {
     <html lang='en'>
       <body className={inter.className}>
         <NavBar />
+        <div className='separator' />
         {children}
       </body>
     </html>
--- components/InfoPage.jsx
+++ components/InfoPage.jsx
@@ -1,16 +1,25 @@
 import '../styles/infoPage.css'
+import banner from '../public/img/banner.jpeg'
+import Image from 'next/image'
 
 export default function DocPage () {
   return (
-    <main className='doc__container'>
-      <section className='doc__text'>
-        <h2>Parque serranías amerrisque </h2>
-        <p>
-          Es un territorio que cuenta con un patrimonio geológico de importancia internacional, es decir, las rocas, minerales y fósiles que ahí se encuentran son reconocidos mundialmente por su singular proceso de formación, edad y otras características.
-          Este patrimonio geológico junto con el natural y cultural, son utilizados para generar el desarrollo sostenible de las comunidades locales, no solo a través del incremento del turismo y la promoción de los productos locales, sino mediante programas de carácter científico y educativo, que acerquen a los habitantes y visitantes del territorio a las ciencias de la tierra y medioambientales.
-        </p>
+    <main className='info'>
+      <section className='info__container'>
+        <section className='info__text__container'>
+          <h2 className='info__text'>Parque serranías amerrisque </h2>
+          <p>
+            Es un territorio que cuenta con un patrimonio geológico de importancia internacional, es decir, las rocas, minerales y fósiles que ahí se encuentran son reconocidos mundialmente por su singular proceso de formación, edad y otras características.
+            Este patrimonio geológico junto con el natural y cultural, son utilizados para generar el desarrollo sostenible de las comunidades locales, no solo a través del incremento.
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt perspiciatis veritatis voluptatum nam quidem eius vel sapiente aut, officiis laudantium accusamus odio, amet optio? Quo temporibus laborum laudantium voluptatum consectetur!
+          </p>
+        </section>
+        <figure className='info__image'>
+          <Image src={banner} alt='banner' fill className='info__image__container' />
+        </figure>
       </section>
-      <figure className='doc__image' />
     </main>
   )
 }
