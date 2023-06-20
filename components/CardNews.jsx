f7d7b11144e59a1ab9307bd57d6d557320edf0ac
Vidal Francisco  Baquedano <devbooster@Vidals-MacBook-Air.local>
2023-06-19 23:25:54 -0600

    news-api-implemented

--- components/CardNews.jsx
+++ components/CardNews.jsx
@@ -1,11 +1,10 @@
 import '../styles/component/cardNews.css'
-import banner from '../public/img/banner.jpeg'
 import calendar from '../public/icons/calendar.png'
 import Image from 'next/image'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 
-export default function News () {
+export default function News ({ title, date, description, banner }) {
   return (
     <section className='news__container'>
       <Image
@@ -16,15 +15,15 @@ export default function News () {
         height={200}
       />
       <article className='news__information'>
-        <h3 className='news__text'>BENDECIDAS MADRES DEL 2023 EN JUIGALPA</h3>
+        <h3 className='news__text'>{title}</h3>
         <figure className='news__date'>
           <Image src={calendar} alt='calendar-icon' width={20} height={20} />
-          <p className='news__text'>16 enero, 2023</p>
+          <p className='news__text'>{date}</p>
           <figure className='news__chevron'>
             <FontAwesomeIcon icon={faChevronRight} className='news__chevron__icon' />
           </figure>
         </figure>
-        <p className='news__text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam fuga vel distinctio possimus voluptatem placeat consequuntur similique aliquid reiciendis magni. Qui ullam, fugit voluptates rem autem itaque ipsa molestiae excepturi. Lorem ipsum dolor sit amet consectetur ad
+        <p className='news__text'>{description}
         </p>
       </article>
     </section>
--- components/NewsPage.jsx
+++ components/NewsPage.jsx
@@ -1,16 +1,21 @@
 import CardNews from './CardNews'
 import '../styles/newsPage.css'
 
-export default function NewsPage () {
+const getNews = async () => {
+  const res = await fetch('http://localhost:8000/api/news/', { cache: 'no-store' })
+  return res.json()
+}
+
+export default async function NewsPage () {
+  const news = await getNews()
   return (
     <main className='news'>
       <section className='news__page__container container'>
         <h2 className='news__title'>Noticias</h2>
         <article className='news__content'>
-          <CardNews />
-          <CardNews />
-          <CardNews />
-          <CardNews />
+          {news.map((item) => (
+            <CardNews key={item.id} {...item} />
+          ))}
         </article>
       </section>
     </main>
